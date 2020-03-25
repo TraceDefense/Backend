7cf426dbf6a7f4ea8c3d119fe04b61ef0b40bac1
Cory Gehr <cogehr@microsoft.com>
2020-03-25 09:54:30 -0700

    Add Swashbuckler for rapid SwaggerDoc generation

--- TraceDefense/TraceDefense.API/Startup.cs
+++ TraceDefense/TraceDefense.API/Startup.cs
@@ -1,18 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 
 namespace TraceDefense.API
 {
+    /// <remarks>
+    /// Ignores missing documentation warnings.
+    /// </remarks>
+#pragma warning disable CS1591
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -26,6 +28,23 @@ namespace TraceDefense.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Add Swagger generator
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Description = "API for communicating between TraceDefense client apps and the backend data processors.",
+                    Title = "TraceDefense",
+                    Version = "v1"
+                });
+
+                var xmlPath = Path.Combine(
+                    AppContext.BaseDirectory,
+                    $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"
+                );
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,4 +63,5 @@ namespace TraceDefense.API
             app.UseMvc();
         }
     }
+#pragma warning restore CS1591
 }
